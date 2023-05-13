020d799f4b19cd597968b307ae83807e155f3258
lancersky <luoyejiang0701@gmail.com>
2023-05-13 19:14:51 +0800

    scripts: improve scripts/bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,9 +11,9 @@ unzip gn.zip
 sudo cp gn /usr/bin/
 
 wget -q https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2
-tar -jxvf gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2 2>/dev/null 1>/dev/null
-echo 'export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb' >> ${HOME}/.bashrc
-echo 'export GNUARMEMB_TOOLCHAIN_PATH="$PWD/gcc-arm-none-eabi-9-2019-q4-major"' >> ${HOME}/.bashrc
+tar -jxf gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2
+echo "export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb" >> ${HOME}/.bashrc
+echo "export GNUARMEMB_TOOLCHAIN_PATH=$PWD/gcc-arm-none-eabi-9-2019-q4-major" >> ${HOME}/.bashrc
 source ${HOME}/.bashrc
 
 pip3 install --user -r zephyr/scripts/requirements.txt
